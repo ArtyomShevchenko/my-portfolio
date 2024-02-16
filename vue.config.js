5defc762435fe6a14fe5e736349393cc460d74f6
ArtyomShevchenko <https://github.com/ArtyomShevchenko>
2024-02-16 15:28:39 +0200

    a

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "",
 });
+
+module.exports = {
+  // publicPath: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "/",
+};
