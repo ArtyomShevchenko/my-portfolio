7c93c8cd76210720c139752284d98b14de714d11
ArtyomShevchenko <https://github.com/ArtyomShevchenko>
2024-02-19 11:25:57 +0200

    deploy

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,4 @@
+import App from "@/App.vue";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import ContactView from "@/views/ContactView.vue";
@@ -5,17 +6,24 @@ import ContactView from "@/views/ContactView.vue";
 export const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: AboutView,
-  },
-  {
-    path: "/contact",
-    name: "contact",
-    component: ContactView,
+    name: "app",
+    component: App,
+    children: [
+      {
+        path: "/",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: AboutView,
+      },
+      {
+        path: "/contact",
+        name: "contact",
+        component: ContactView,
+      },
+    ],
   },
 ];
